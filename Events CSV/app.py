bc96f8dc2c2d867ff7b41e43c930c6ccbe8a65ce
oawwad <71391170+optimizca@users.noreply.github.com>
2021-02-08 20:46:11 -0500

    Update app.py

--- Events CSV/app.py
+++ Events CSV/app.py
@@ -23,69 +23,68 @@ try:
     # uncomment to re-add python 3 .timestamp
     #start = math.trunc(datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S').timestamp() * 1000)
     #end = math.trunc(datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S').timestamp() * 1000) 
-
-    # For python 2
-    start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
-    start = int(time.mktime(start_time.utctimetuple()) * 1000 + start_time.microsecond / 1000)
-
-    end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
-    end = int(time.mktime(end_time.utctimetuple()) * 1000 + end_time.microsecond / 1000)
-
-    url = baseurl + "/controller/rest/applications/" + str(application_id) + "/events?summary=" + summary + \
-                    "&event-types=" + event_types + \
-                    "&severities=" + severities + \
-                    "&time-range-type=BETWEEN_TIMES&start-time=" + str(start) + \
-                    "&end-time=" + str(end) + \
-                    "&output=JSON"
-
-    # Encodes the auth in base64
-    auth = user + "@" + account + ":" + password
-    auth_encode= base64.b64encode(memoryview(bytes(auth, 'utf8')))
-    fixed_auth = str(auth_encode).replace('b', '', 1).replace("'", '')
-    
-    headers = {
-        "Authorization": "Basic " + fixed_auth
-    }
-    r = requests.get(url, headers=headers)
-
-    if r.status_code == 200:
-        jsons = r.json()
-        with open('output.csv', mode='w') as events:
-            events_writer = csv.writer(events, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            events_writer.writerow(['archived', 
-                                    'deepLinkUrl', 
-                                    'eventTime', 
-                                    'id',
-                                    'markedAsRead', 
-                                    'markedAsResolved', 
-                                    'severity', 
-                                    'subType', 
-                                    'summary',
-                                    'triggeredEntity', 
-                                    'type', 
-                                    'entityType', 
-                                    'name', 
-                                    'entitiyId'])
-            for i in jsons:
-                # Pretty print each result
-                #print(json.dumps(i, indent=4, sort_keys=True))
-                for j in i['affectedEntities']:
-                    events_writer.writerow([i['archived'], 
-                                            i['deepLinkUrl'],
-                                            i['eventTime'], 
-                                            i['id'], 
-                                            i['markedAsRead'], 
-                                            i['markedAsResolved'], 
-                                            i['severity'], 
-                                            i['subType'], 
-                                            i['summary'], 
-                                            i['triggeredEntity'], 
-                                            i['type'], 
-                                            j['entityType'], 
-                                            j['name'], 
-                                            j['entityId']])
-    else:
-        print("Status code: " + r.status_code)
-            
 except Exception as e:
     print(e)
+    # For python 2
+start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+start = int(time.mktime(start_time.utctimetuple()) * 1000 + start_time.microsecond / 1000)
+
+end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+end = int(time.mktime(end_time.utctimetuple()) * 1000 + end_time.microsecond / 1000)
+
+url = baseurl + "/controller/rest/applications/" + application_id + "/events?summary=" + summary + \
+                "&event-types=" + event_types + \
+                "&severities=" + severities + \
+                "&time-range-type=BETWEEN_TIMES&start-time=" + str(start) + \
+                "&end-time=" + str(end) + \
+                "&output=JSON"
+
+# Encodes the auth in base64
+auth = user + "@" + account + ":" + password
+auth_encode= base64.b64encode(memoryview(bytes(auth, 'utf8')))
+fixed_auth = str(auth_encode).replace('b', '', 1).replace("'", '')
+
+headers = {
+    "Authorization": "Basic " + fixed_auth
+}
+r = requests.get(url, headers=headers)
+
+if r.status_code == 200:
+    jsons = r.json()
+    with open('output.csv', mode='w') as events:
+        events_writer = csv.writer(events, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        events_writer.writerow(['archived', 
+                                'deepLinkUrl', 
+                                'eventTime', 
+                                'id',
+                                'markedAsRead', 
+                                'markedAsResolved', 
+                                'severity', 
+                                'subType', 
+                                'summary',
+                                'triggeredEntity', 
+                                'type', 
+                                'entityType', 
+                                'name', 
+                                'entitiyId'])
+        for i in jsons:
+            # Pretty print each result
+            #print(json.dumps(i, indent=4, sort_keys=True))
+            for j in i['affectedEntities']:
+                events_writer.writerow([i['archived'], 
+                                        i['deepLinkUrl'],
+                                        i['eventTime'], 
+                                        i['id'], 
+                                        i['markedAsRead'], 
+                                        i['markedAsResolved'], 
+                                        i['severity'], 
+                                        i['subType'], 
+                                        i['summary'], 
+                                        i['triggeredEntity'], 
+                                        i['type'], 
+                                        j['entityType'], 
+                                        j['name'], 
+                                        j['entityId']])
+else:
+    print("Status code: " + str(r.status_code))
+        
